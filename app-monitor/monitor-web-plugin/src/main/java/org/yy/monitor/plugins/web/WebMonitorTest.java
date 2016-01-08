f8c2559e617be1fdbba6d58841328a64a41d77c8
zl2006 <zl2006@gmail.com>
2016-01-08 10:48:29 +0800

    修复webutils所在包

--- app-monitor/monitor-web-plugin/src/main/java/org/yy/monitor/plugins/web/WebMonitorTest.java
+++ app-monitor/monitor-web-plugin/src/main/java/org/yy/monitor/plugins/web/WebMonitorTest.java
@@ -13,7 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.yy.framework.util.http.WebUtils;
+import org.yy.framework.util.net.WebUtils;
 import org.yy.monitor.core.MonitorTest;
 import org.yy.monitor.core.entity.Entity;
 import org.yy.monitor.core.entity.EntityItem;
